4212112b4b0c767a68f9767d7070fef63176b451
Kucha1122 <uyu1@o2.pl>
2017-08-31 17:36:40 +0200

    api

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { WeatherProvider } from '../../providers/weather/weather';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
@@ -13,19 +14,28 @@ export class HomePage {
     country:string;
   }
 
-  constructor(public navCtrl: NavController, private weatherProvider: WeatherProvider) {
+  constructor(
+    public navCtrl: NavController, 
+    private weatherProvider: WeatherProvider,
+    private storage:Storage) {
 
   }
 
   ionViewWillEnter(){
-    this.location = {
-      city: 'Lublin',
-      country: 'PL'
-    }
+    this.storage.get('location').then((val) => {
+      if(val != null){
+        this.location = JSON.parse(val);
+      }else{
+        this.location = {
+          city: 'Lublin',
+          country: 'PL'
+        }
+      }
 
     this.weatherProvider.getWeather(this.location.city, this.location.country)
-    .subscribe(weather => {
-      this.weather = weather.current_observation;
+      .subscribe(weather => {
+        this.weather = weather.current_observation;
+      });
     });
   }
 
--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the SettingsPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import { Storage } from '@ionic/storage';
+import { HomePage } from '../home/home';
 
 @IonicPage()
 @Component({
@@ -14,12 +9,36 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'settings.html',
 })
 export class SettingsPage {
+  city:string;
+  country:string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private storage:Storage) {
+
+      this.storage.get('location').then((val) => {
+        if(val != null){
+          let location = JSON.parse(val);
+          this.city = location.city;
+          this.country = location.country;
+        }else{
+          this.city = 'Lublin';
+          this.country = 'PL';
+        }
+      });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SettingsPage');
   }
 
+  saveForm(){
+    let location = {
+      city: this.city,
+      country: this.country
+    }
+    this.storage.set('location', JSON.stringify(location));
+    this.navCtrl.push(HomePage);
+  }
 }
